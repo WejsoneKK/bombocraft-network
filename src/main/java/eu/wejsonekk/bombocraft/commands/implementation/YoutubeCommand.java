da8f58fe7f984f204482cc62594c53819d141c48
WejsoneKK <108413968+WejsoneKK@users.noreply.github.com>
2024-07-05 21:36:24 +0200

    Create YoutubeCommand.java

---
+++ src/main/java/eu/wejsonekk/bombocraft/commands/implementation/YoutubeCommand.java
@@ -0,0 +1,33 @@
+package eu.wejsonekk.bombocraft.commands.implementation;
+
+import dev.rollczi.litecommands.annotations.command.Command;
+import dev.rollczi.litecommands.annotations.context.Context;
+import dev.rollczi.litecommands.annotations.execute.Execute;
+import eu.wejsonekk.bombocraft.configuration.implementation.MessageConfiguration;
+import eu.wejsonekk.bombocraft.notification.NotificationAnnouncer;
+import org.bukkit.entity.Player;
+
+@Command(name = "youtube", aliases = {"yt"})
+public class YoutubeCommand {
+
+    private MessageConfiguration messages;
+    private NotificationAnnouncer announcer;
+
+    public YoutubeCommand(MessageConfiguration messages, NotificationAnnouncer announcer) {
+        this.messages = messages;
+        this.announcer = announcer;
+    }
+
+    @Execute
+    void executeYoutube(@Context Player player){
+
+        this.messages.standardCommands.youtubeCommand
+                .stream()
+                .forEach(messages -> {
+                    this.announcer.sendMessage(player, messages);
+                });
+
+
+
+    }
+}
