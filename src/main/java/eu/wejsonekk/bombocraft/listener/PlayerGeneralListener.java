dad9f3c91d3d596cdde5fc2a13d242a9db7d07b2
WejsoneKK <108413968+WejsoneKK@users.noreply.github.com>
2024-07-05 15:26:53 +0200

    Update PlayerGeneralListener

--- src/main/java/eu/wejsonekk/bombocraft/listener/PlayerGeneralListener.java
+++ src/main/java/eu/wejsonekk/bombocraft/listener/PlayerGeneralListener.java
@@ -1,4 +1,20 @@
 package eu.wejsonekk.bombocraft.listener;
 
-public class PlayerGeneralListener {
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class PlayerGeneralListener implements Listener {
+
+
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event){
+
+    }
+    @EventHandler
+    public void onPlayerLeave(PlayerQuitEvent event){
+
+    }
 }
