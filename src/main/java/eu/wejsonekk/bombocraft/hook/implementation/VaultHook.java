6ccfb62825149ea6ddb57adb5fa0dba56afae012
WejsoneKK <108413968+WejsoneKK@users.noreply.github.com>
2024-07-05 15:26:26 +0200

    Add Vault Integration

---
+++ src/main/java/eu/wejsonekk/bombocraft/hook/implementation/VaultHook.java
@@ -0,0 +1,36 @@
+package eu.wejsonekk.bombocraft.hook.implementation;
+
+import eu.wejsonekk.starblock.hook.Hook;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.Server;
+import org.bukkit.plugin.RegisteredServiceProvider;
+
+public class VaultHook implements Hook {
+
+    private final Server server;
+    private Economy economy;
+
+    public VaultHook(Server server) {
+        this.server = server;
+    }
+
+    @Override
+    public void initialize() {
+        RegisteredServiceProvider<Economy> economyProvider = this.server.getServicesManager().getRegistration(Economy.class);
+
+        if (economyProvider == null) {
+            throw new IllegalStateException("Vault founded, but you don't have a plugin that supports economy");
+        }
+
+        this.economy = economyProvider.getProvider();
+    }
+
+    @Override
+    public String pluginName() {
+        return "Vault";
+    }
+
+    public Economy getEconomy() {
+        return this.economy;
+    }
+}
