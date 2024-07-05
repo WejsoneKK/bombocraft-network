61808f78bb58271eb015f5a8c6cc62eb0935cc90
WejsoneKK <108413968+WejsoneKK@users.noreply.github.com>
2024-07-05 15:24:24 +0200

    Fixed BomboStoreGui and BomboStoreCommand

--- src/main/java/eu/wejsonekk/bombocraft/commands/implementation/BomboStoreCommand.java
+++ src/main/java/eu/wejsonekk/bombocraft/commands/implementation/BomboStoreCommand.java
@@ -2,6 +2,7 @@ package eu.wejsonekk.bombocraft.commands.implementation;
 
 import dev.rollczi.litecommands.annotations.command.Command;
 import dev.rollczi.litecommands.annotations.context.Context;
+import dev.rollczi.litecommands.annotations.execute.Execute;
 import eu.wejsonekk.bombocraft.feature.shop.BomboStoreGui;
 import org.bukkit.entity.Player;
 
@@ -10,8 +11,12 @@ public class BomboStoreCommand {
 
     private BomboStoreGui bomboStoreGui;
 
+    public BomboStoreCommand(BomboStoreGui bomboStoreGui) {
+        this.bomboStoreGui = bomboStoreGui;
+    }
 
-    void openBomboSore(@Context Player player){
-
+    @Execute
+    void OpenBomboStore(@Context Player player){
+        this.bomboStoreGui.openInventory(player);
     }
 }
