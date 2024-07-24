a6f149c932ec899be1ee272a45298156b10f7838
WejsoneKK <108413968+WejsoneKK@users.noreply.github.com>
2024-07-24 15:41:23 +0200

    Update AutoMessage.java

--- src/main/java/eu/wejsonekk/bombocraft/commands/implementation/BomboStoreCommand.java
+++ src/main/java/eu/wejsonekk/bombocraft/commands/implementation/BomboStoreCommand.java
@@ -3,20 +3,13 @@ package eu.wejsonekk.bombocraft.commands.implementation;
 import dev.rollczi.litecommands.annotations.command.Command;
 import dev.rollczi.litecommands.annotations.context.Context;
 import dev.rollczi.litecommands.annotations.execute.Execute;
-import eu.wejsonekk.bombocraft.feature.shop.BomboStoreGui;
 import org.bukkit.entity.Player;
 
 @Command(name = "bombostore", aliases = {"bombosklep", "bomboshop", "bomboostore"})
 public class BomboStoreCommand {
 
-    private BomboStoreGui bomboStoreGui;
-
-    public BomboStoreCommand(BomboStoreGui bomboStoreGui) {
-        this.bomboStoreGui = bomboStoreGui;
-    }
-
     @Execute
     void OpenBomboStore(@Context Player player){
-        this.bomboStoreGui.openInventory(player);
+
     }
 }
